9756f5fea4fbc784ec64659dfbc8ef3eb157fc59
nuoxi4n <nuo_xian@qq.com>
2026-05-21 00:13:58 +0800

    fix: 修改测试环境为 nuxt，以支持 E2E 测试

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'node',
+    environment: 'nuxt',
     include: ['tests/e2e/**/*.test.ts'],
     setupFiles: ['tests/e2e/setup/env.ts'],
     fileParallelism: false,
