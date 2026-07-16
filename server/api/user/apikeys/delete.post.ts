f1708024338ac6f7fae291537b8a01d62063ce94
nuoxi4n <nuo_xian@qq.com>
2026-07-17 05:17:46 +0800

    refactor(api): trim redundant user handler types

--- server/api/user/apikeys/delete.post.ts
+++ server/api/user/apikeys/delete.post.ts
@@ -1,11 +1,10 @@
-import type { H3Event } from 'h3'
 import { createError } from 'h3'
 import { idSchema } from '~~/server/schemas/common'
 import { apiKeyService } from '~~/server/services/api-key-service'
 import { defineAuthenticatedEventHandler } from '~~/server/utils/auth'
 import { readZodBody } from '~~/server/utils/zod'
 
-export default defineAuthenticatedEventHandler(async (event: H3Event, user) => {
+export default defineAuthenticatedEventHandler(async (event, user) => {
   const { id } = await readZodBody(event, idSchema)
 
   const deleted = await apiKeyService.deleteForUser(user.id, id)
